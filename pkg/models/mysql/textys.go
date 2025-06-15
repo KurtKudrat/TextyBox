6b1eeb591d95ee41b0cec6491c9e80eeac31a6e3
K Kudret <kurt@Ks-MacBook-Pro-2.local>
2025-06-15 15:42:15 -0500

    adding data module

---
+++ pkg/models/mysql/textys.go
@@ -0,0 +1,23 @@
+package mysql
+
+import (
+	"database/sql"
+
+	"github.com/kurt/Learning/Golang/Project/textybox/pkg/models"
+)
+
+type TextyModel struct {
+	DB *sql.DB
+}
+
+func (m *TextyModel) Insert(title, content, expires string) (int, error) {
+	return 0, nil
+}
+
+func (m *TextyModel) Get(id int) (*models.Texty, error) {
+	return nil, nil
+}
+
+func (m *TextyModel) Latest() ([]*models.Texty, error) {
+	return nil, nil
+}
