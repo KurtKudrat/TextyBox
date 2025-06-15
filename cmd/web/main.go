fac8069b13ed37b13b221dd2414f0ad7b955a117
K Kudret <kurt@Ks-MacBook-Pro-2.local>
2025-06-15 14:46:54 -0500

    adding docker compose and set up SQL connnection and DB

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,17 +17,18 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@tcp(127.0.0.1:3306)/texty?parseTime=True", "MySQL database")
 	flag.Parse()
-
-	// f, err := os.OpenFile("/temp/Log.log", os.O_RDWR|os.O_CREATE, 0666)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer f.Close()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
@@ -40,5 +44,15 @@ func main() {
 	// err := http.ListenAndServe(*addr, mux)
 	erro := srv.ListenAndServe()
 	errorLog.Fatal(erro)
+}
 
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
